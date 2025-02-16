7387744dd7053acb3e963aa61050d307ead3222f
andrei-g-lupu <andreigeorgelupu@gmail.com>
2025-02-16 16:35:03 +0200

    Really fixed build problems for 0.2.1

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,15 @@
 'use client';
 
-import React from 'react';
+import React, { Suspense } from 'react';
 import Login from '../components/Login';
 import styles from '../components/Login.module.css'; // Ensure the path is correct
 
 const LoginPage = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <Login />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Login />
+      </Suspense>
     </div>
   );
 };
